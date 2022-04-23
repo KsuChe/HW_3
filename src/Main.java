de39af29c369ea0bdc9bc8ccc7c797ba6c4451db
KsuChe <nerro@mail.ru>
2022-04-23 14:14:06 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
+// task 1
+int clientOS = 1;
+if (clientOS ==1) {
+    System.out.println("Установите версию приложения для Android по ссылке");
+} else {
+    System.out.println("Установите версию приложения для iOS по ссылке");
+}
     }
 }
